3dac92371fabaf98141975e85ba5b9cb38b91176
jarlvs <premshinde13@gmail.com>
2018-06-20 11:53:40 +0530

    implement coding style

--- process.js
+++ process.js
@@ -5,16 +5,16 @@
       
      alert(rating)
      $.ajax({
-       type: "POST",
-       url: "process.php",
-       data: {rating : rating },
-       success: function() {
-        $('#contact_form').html("<div id='message' style='text-align:center'></div>");
+         type: "POST",
+         url: "process.php",
+         data: {rating : rating },
+         success: function() {
+                   $('#contact_form').html("<div id='message' style='text-align:center'></div>");
       
-       $('#message').html("<img style='width:15%' id='checkmark' src='check.png' />")
-        .append("<h2 style='font-size:50px;'>Thank you for your feedback</h2>")
-        .append("<p style='font-size:40px;'>It means a lot to us</p>")
-       }
+                   $('#message').html("<img style='width:15%' id='checkmark' src='check.png' />")
+                   .append("<h2 style='font-size:50px;'>Thank you for your feedback</h2>")
+                   .append("<p style='font-size:40px;'>It means a lot to us</p>")
+          }
    
      });
    
